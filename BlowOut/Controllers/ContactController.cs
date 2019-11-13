9c9d7ca7ef8fc3a5e9dc3682b5eb183b210c3d23
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2019-11-13 15:35:40 -0700

    Got the email working

--- BlowOut/Controllers/ContactController.cs
+++ BlowOut/Controllers/ContactController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace BlowOut.Controllers
@@ -16,9 +19,41 @@ namespace BlowOut.Controllers
             return View();
         }
 
+        //the route receives an email parameter first to handle user errors with the closing slash '/'
         public ActionResult Email(string email, string name)
         {
-            ViewBag.Email = "Thank you, " + name + ". We will email you at <u>" + email + "</u>";
+            //make sure email is valid
+            if (email.Contains('@'))
+            {
+                ViewBag.Email = "Thank you, " + name + ". We will email you at <u>" + email + "</u>";
+
+                //write body message
+                string body = name + ", <br><br>Thank you for your interest. We will be in touch shortly.<br><br> Gary Anderson<br>BlowOut CEO";
+
+                //set up email message
+                MailMessage mail = new MailMessage();
+                mail.To.Add(email);
+                mail.From = new MailAddress("profgaryanderson@gmail.com");
+                mail.Subject = "BlowOut Contact";
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+                //set up smtp client
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("profgaryanderson@gmail.com", "Magsungay17");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+            }
+            //handle exception
+            else
+            {
+                ViewBag.Email = "Please enter a valid email address. Thank you.";
+            }
+            
+
             return View("Index");
         }
     }
