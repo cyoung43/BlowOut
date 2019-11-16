982794c212cab5dffd88adb9f15635a44da8acb2
cmcleod2 <56420438+cmcleod2@users.noreply.github.com>
2019-11-15 23:02:28 -0700

    Changes in Instrument Contoller and All Instrument Views

--- BlowOut/Controllers/InstrumentController.cs
+++ BlowOut/Controllers/InstrumentController.cs
@@ -14,9 +14,33 @@ namespace BlowOut.Controllers
             return View();
         }
 
+        public ActionResult TrumpetNew()
+        {
+            ViewBag.PriceInfo = "$55 a month";
+            return View("Trumpet");
+        }
+
+        public ActionResult TrumpetUsed()
+        {
+            ViewBag.PriceInfo = "$25 a month";
+            return View("Trumpet");
+        }
+
         public ActionResult Tuba()
         {
             return View();
+        }
+
+        public ActionResult TubaNew()
+        {
+            ViewBag.PriceInfo = "$70 a month";
+            return View("Tuba");
+        }
+
+        public ActionResult TubaUsed()
+        {
+            ViewBag.PriceInfo = "$50 a month";
+            return View("Tuba");
         }
 
         public ActionResult Trombone()
@@ -24,9 +48,33 @@ namespace BlowOut.Controllers
             return View();
         }
 
+        public ActionResult TromboneNew()
+        {
+            ViewBag.PriceInfo = "$60 a month";
+            return View("Trombone");
+        }
+
+        public ActionResult TromboneUsed()
+        {
+            ViewBag.PriceInfo = "$35 a month";
+            return View("Trombone");
+        }
+
         public ActionResult Clarinet()
         {
             return View();
+        }
+
+        public ActionResult ClarinetNew()
+        {
+            ViewBag.PriceInfo = "$35 a month";
+            return View("Clarinet");
+        }
+
+        public ActionResult ClarinetUsed()
+        {
+            ViewBag.PriceInfo = "$27 a month";
+            return View("Clarinet");
         }
 
         public ActionResult Flute()
@@ -34,9 +82,33 @@ namespace BlowOut.Controllers
             return View();
         }
 
+        public ActionResult FluteNew()
+        {
+            ViewBag.PriceInfo = "$40 a month";
+            return View("Flute");
+        }
+
+        public ActionResult FluteUsed()
+        {
+            ViewBag.PriceInfo = "$25 a month";
+            return View("Flute");
+        }
+
         public ActionResult Saxophone()
         {
             return View();
+        }
+
+        public ActionResult SaxophoneNew()
+        {
+            ViewBag.PriceInfo = "$42 a month";
+            return View("Saxophone");
+        }
+
+        public ActionResult SaxophoneUsed()
+        {
+            ViewBag.PriceInfo = "$30 a month";
+            return View("Saxophone");
         }
     }
 }
